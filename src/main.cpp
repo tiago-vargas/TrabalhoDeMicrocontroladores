e84fa71488b53d9867fe990f2b178fd45ed614e8
Tiago Vargas <tiago.vargaspo@proton.me>
2025-02-15 11:02:49 -0300

    Ultrasonic module example
    From tutorial:
    https://randomnerdtutorials.com/esp32-hc-sr04-ultrasonic-arduino/

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,56 @@
+/*********
+Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-hc-sr04-ultrasonic-arduino/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int trigPin = 5;
+const int echoPin = 18;
 
-void setup()
-{
-	// put your setup code here, to run once:
-	int result = myFunction(2, 3);
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+void setup() {
+	Serial.begin(115200); // Starts the serial communication
+	pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+	pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 }
 
-void loop()
-{
-	// put your main code here, to run repeatedly:
-}
+void loop() {
+	// Clears the trigPin
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
+	// Sets the trigPin on HIGH state for 10 micro seconds
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-	return x + y;
+	// Reads the echoPin, returns the sound wave travel time in microseconds
+	duration = pulseIn(echoPin, HIGH);
+
+	// Calculate the distance
+	distanceCm = duration * SOUND_SPEED/2;
+
+	// Convert to inches
+	distanceInch = distanceCm * CM_TO_INCH;
+
+	// Prints the distance in the Serial Monitor
+	Serial.print("Distance (cm): ");
+	Serial.println(distanceCm);
+	Serial.print("Distance (inch): ");
+	Serial.println(distanceInch);
+
+	delay(1000);
 }
